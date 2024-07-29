99b207c38d90dbe6630c1179f77dab7184a061e7
VaneCarreras <vanecarreras91@gmail.com>
2024-07-29 08:16:15 -0300

    frontSumar

---
+++ Controllers/ConsultasController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LegalSoft.Models;
+
+namespace LegalSoft.Controllers;
+
+public class ConsultasController : Controller
+{
+    private readonly ILogger<ConsultasController> _logger;
+
+    public ConsultasController(ILogger<ConsultasController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    // public IActionResult Error()
+    // {
+    //     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    // }
+}
